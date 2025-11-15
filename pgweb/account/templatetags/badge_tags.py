440ff064cf1d8c40a2a439a45a33f9442e08eb59
Andrey Borodin <amborodin@acm.org>
2025-11-16 00:02:43 +0500

    Add badge_tags template filter for badge system

---
+++ pgweb/account/templatetags/badge_tags.py
@@ -0,0 +1,17 @@
+from django import template
+
+register = template.Library()
+
+
+@register.filter
+def get_item(dictionary, key):
+    """
+    Template filter to get item from dictionary by key.
+    Usage: {{ mydict|get_item:key }}
+    """
+    if dictionary is None:
+        return None
+    return dictionary.get(key)
+
+
+
